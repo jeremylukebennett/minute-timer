85eca78b43ad42873a5ccf67cf2cb2be864188b7
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-07-11 14:24:22 -0500

    working on react doc version of the clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Timer from './components/Timer.js'
+import Countdown from './components/Countdown.js'
+import Clock from './components/Clock.js'
 
 class App extends Component {
 
@@ -13,7 +15,9 @@ class App extends Component {
     return (
       <div>
         <h2>Minute Timer</h2>
-        <Timer/>
+        {/* <Timer/> */}
+        {/* <Countdown/> */}
+        <Clock date={new Date()} />
       </div>
     );
   }
---
+++ src/components/Countdown.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Countdown extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
+
+    
+    render(){
+
+
+        return(         
+            <div>
+                <h2>Countdown</h2>
+            </div>
+        )
+    }
+}
+
+export default Countdown;
